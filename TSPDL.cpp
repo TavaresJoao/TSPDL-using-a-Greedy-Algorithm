afd2a3f0e91c7a8370b711998882111f66182909
Joao Tavares <joao_stavares@outlook.com>
2018-09-12 13:06:55 -0300

    mudei o make_greedy_trade e fiz o make_random_trade

--- TSPDL.cpp
+++ TSPDL.cpp
@@ -15,7 +15,7 @@ int make_greedy_trade(string inFile, string outFile, int inputFile_type)
 
   clock_t t;
   t = clock();
-  pair<int*,int> trade = greedy_algorithm(myproblem, L0);
+  pair<vector<int>, int> trade = greedy_algorithm(myproblem, L0);
   t = clock()-t;
 
   //testes
@@ -28,14 +28,51 @@ int make_greedy_trade(string inFile, string outFile, int inputFile_type)
 
   if( export_data(outFile, inFile, trade, (double)t/CLOCKS_PER_SEC) )
   {
-    free_Vector(trade.first);
     free_TSPDL(myproblem);
 
     cout << "Problem opening the output file" << endl;
     return 1; // ERROR
   }
 
-  free_Vector(trade.first);
+  free_TSPDL(myproblem);
+  return 0;
+}
+
+int make_random_trade(string inFile, string outFile, int inputFile_type)
+{
+  TSPDL *myproblem = import_data(inFile, inputFile_type);
+  if(myproblem == NULL)
+  {
+    cout << "Problem opening the input file" << endl;
+    return 1; // ERROR
+  }
+
+  int i, L0=0;
+  for(i=0; i<myproblem->N; i++)
+    L0 += myproblem->setPorts[i].demand;
+
+  clock_t t;
+  t = clock();
+  pair<vector<int>, int> trade = random_algorithm(myproblem, L0);
+  t = clock()-t;
+
+  //testes
+  /*
+  cout << "Custo total: " << trade.second << endl << "Rota: ";
+  for(i=0; i<myproblem->N; i++)
+    cout << trade.first[i] << " ";
+  cout << endl << "Tempo: " << (double)t/CLOCKS_PER_SEC << "s" << endl;
+  */
+
+
+  if( export_data(outFile, inFile, trade, (double)t/CLOCKS_PER_SEC) )
+  {
+    free_TSPDL(myproblem);
+
+    cout << "Problem opening the output file" << endl;
+    return 1; // ERROR
+  }
+
   free_TSPDL(myproblem);
   return 0;
 }
