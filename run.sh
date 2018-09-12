d952ee902c571b594e6d667bba21c0818d6ff620
Joao Tavares <joao_stavares@outlook.com>
2018-09-12 13:09:09 -0300

    script shell alterado para compilar todos os arquivos

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ g++ -c file.cpp
 g++ -c Greedy.cpp
 g++ -c main.cpp
 g++ -c TSPDL.cpp
-g++ -o main allocation.o file.o Greedy.o main.o TSPDL.o
+g++ -c Random.cpp
+g++ -o main allocation.o file.o Greedy.o main.o TSPDL.o Random.o
 
 for y in Instances/TSPDL_Instances/*
 do
@@ -19,5 +20,5 @@ done
 for y in Instances/tspdl-test-data-equal-demand/*
 do
   echo $y;
-  ./main $y Outputs/myoutput_1.out 1
+  ./main $y Respostas/myoutput_1.out 1
 done
