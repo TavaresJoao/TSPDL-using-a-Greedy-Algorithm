fa07bf7ff87e8e35c21a0b6936eb0ffc9cc64126
Joao Tavares <joao_stavares@outlook.com>
2018-09-12 13:08:33 -0300

    código da main com a rota randõmica

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 
 int main(int argc, char *argv[])
 {
+  /*
+    argv[1] == input_file
+    argv[2] == output_file
+    argv[3] == input_type_file
+  */
   if(argc == 4)
   {
-    return make_greedy_trade(argv[1], argv[2], stoi(argv[3]));
+    return make_random_trade(argv[1], argv[2], stoi(argv[3]));
   }
 
   return 0;
